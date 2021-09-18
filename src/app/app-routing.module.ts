e07e378701a51fab04d999d4a95cdc51b197688c
Darren Fernandes <darrenmfdes@gmail.com>
2021-09-18 08:36:49 +0530

    dfs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { FileUploadComponent } from './sec-master-upload/file-upload/file-upload.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"upload",component:FileUploadComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UploadService } from './sec-master-upload/shared/services/upload.service';
+import { SecMasterUploadModule } from './sec-master-upload/sec-master-upload.module';
+import { SecMasterViewModule } from './sec-master-view/sec-master-view.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DrawerComponent } from './drawer/drawer.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import { MatIconModule} from '@angular/material/icon'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SecMasterUploadModule,
+    SecMasterViewModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [UploadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
